489c196305f38a19687adb9defff143145d2c70d
mohayonao <mohayonao@gmail.com>
2015-07-09 23:30:02 +0900

    add Operator

---
+++ src/Operator.js
@@ -0,0 +1,90 @@
+export const CONTEXT = typeof Symbol !== "undefined" ? Symbol("CONTEXT") : "_@mohayonao/operator:CONTEXT";
+export const OUTLET = typeof Symbol !== "undefined" ? Symbol("OUTLET") : "_@mohayonao/operator:OUTLET";
+export const OSCILLATOR = typeof Symbol !== "undefined" ? Symbol("OSCILLATOR") : "_@mohayonao/operator:OSCILLATOR";
+export const GAIN = typeof Symbol !== "undefined" ? Symbol("GAIN") : "_@mohayonao/operator:GAIN";
+export const ENVELOPES = typeof Symbol !== "undefined" ? Symbol("ENVELOPES") : "_@mohayonao/operator:ENVELOPES";
+
+export default class Operator {
+  constructor(audioContext) {
+    this[CONTEXT] = audioContext;
+    this[OSCILLATOR] = audioContext.createOscillator();
+    this[GAIN] = audioContext.createGain();
+    this[OUTLET] = this[GAIN];
+    this[ENVELOPES] = {};
+  }
+
+  get context() {
+    return this[CONTEXT];
+  }
+
+  get outlet() {
+    return this[OUTLET];
+  }
+
+  get type() {
+    return this[OSCILLATOR].type;
+  }
+
+  set type(value) {
+    this[OSCILLATOR].type = value;
+  }
+
+  get frequency() {
+    return this[OSCILLATOR].frequency;
+  }
+
+  get detune() {
+    return this[OSCILLATOR].detune;
+  }
+
+  get onended() {
+    return this[OSCILLATOR].onended;
+  }
+
+  set onended(value) {
+    this[OSCILLATOR].onended = value;
+  }
+
+  get gain() {
+    return this[GAIN].gain;
+  }
+
+  connect(destination) {
+    this[OSCILLATOR].connect(this[GAIN]);
+    this[GAIN].connect(destination);
+  }
+
+  disconnect(...args) {
+    this[OSCILLATOR].disconnect();
+    this[GAIN].disconnect(...args);
+  }
+
+  start(when) {
+    applyTo(this[ENVELOPES].frequency, this[OSCILLATOR].frequency, when);
+    applyTo(this[ENVELOPES].detune, this[OSCILLATOR].detune, when);
+    applyTo(this[ENVELOPES].gain, this[GAIN].gain, when);
+    this[OSCILLATOR].start(when);
+  }
+
+  stop(when) {
+    this[OSCILLATOR].stop(when);
+  }
+
+  setPeriodicWave(periodicWave) {
+    this[OSCILLATOR].setPeriodicWave(periodicWave);
+  }
+
+  setEnvelope(envelope, target = "gain") {
+    this[ENVELOPES][target] = envelope;
+  }
+
+  getEnvelope(target = "gain") {
+    return this[ENVELOPES][target];
+  }
+}
+
+function applyTo(envelope, audioParam, startTime) {
+  if (envelope && typeof envelope.applyTo === "function") {
+    envelope.applyTo(audioParam, startTime);
+  }
+}
---
+++ test/Operator.js
@@ -0,0 +1,169 @@
+import "web-audio-test-api";
+import assert from "power-assert";
+import sinon from "sinon";
+import Envelope from "@mohayonao/envelope";
+import Operator, { OSCILLATOR, GAIN } from "../src/Operator";
+
+describe("Operator", () => {
+  let audioContext;
+
+  beforeEach(() => {
+    audioContext = new global.AudioContext();
+  });
+
+  describe("constructor()", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      assert(op instanceof Operator);
+    });
+  });
+  describe("#context: AudioContext", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      assert(op.context === audioContext);
+    });
+  });
+  describe("#outlet: GainNode", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      assert(op.outlet === op[GAIN]);
+    });
+  });
+  describe("#type: string", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      op.type = "triangle";
+
+      assert(op.type === op[OSCILLATOR].type);
+    });
+  });
+  describe("#frequency: AudioParam", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      assert(op.frequency === op[OSCILLATOR].frequency);
+    });
+  });
+  describe("#detune: AudioParam", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      assert(op.detune === op[OSCILLATOR].detune);
+    });
+  });
+  describe("#onended: function", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      op.onended = sinon.spy();
+
+      assert(op.onended === op[OSCILLATOR].onended);
+    });
+  });
+  describe("#gain: AudioParam", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      assert(op.gain === op[GAIN].gain);
+    });
+  });
+  describe("#connect(destination): void", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      op.connect(audioContext.destination);
+
+      assert(audioContext.destination.$isConnectedFrom(op[GAIN]));
+      assert(op[GAIN].$isConnectedFrom(op[OSCILLATOR]));
+    });
+  });
+  describe("#disconnect(destination): void", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      op.connect(audioContext.destination);
+      op.disconnect();
+
+      assert(!audioContext.destination.$isConnectedFrom(op[GAIN]));
+      assert(!op[GAIN].$isConnectedFrom(op[OSCILLATOR]));
+    });
+  });
+  describe("#start(when): void", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      op.start(1);
+
+      assert(op[OSCILLATOR].$startTime === 1);
+    });
+  });
+  describe("#stop(when): void", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+
+      op.start(1);
+      op.stop(2);
+
+      assert(op[OSCILLATOR].$stopTime === 2);
+    });
+  });
+  describe("#setPeriodicWave(periodicWave: PeriodicWave): void", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+      let wave = audioContext.createPeriodicWave(new Float32Array(1024), new Float32Array(1024));
+
+      op.setPeriodicWave(wave);
+
+      assert(op.type === "custom");
+      assert(op[OSCILLATOR].$custom === wave);
+    });
+  });
+  describe("#setEnvelope(envelope: any, target: string = 'gain'): void", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+      let envelope1 = Envelope.asr(0.2, 0.5, 0.3);
+      let envelope2 = envelope1.madd(5);
+      let envelope3 = envelope2.madd(10, 880);
+
+      envelope1.applyTo = sinon.spy(envelope1.applyTo.bind(envelope1));
+      envelope2.applyTo = sinon.spy(envelope2.applyTo.bind(envelope2));
+      envelope3.applyTo = sinon.spy(envelope3.applyTo.bind(envelope3));
+
+      op.setEnvelope(envelope1);
+      op.setEnvelope(envelope2, "detune");
+      op.setEnvelope(envelope3, "frequency");
+      op.start(1);
+
+      assert(envelope1.applyTo.callCount === 1);
+      assert(envelope1.applyTo.args[0][0] === op[GAIN].gain);
+      assert(envelope1.applyTo.args[0][1] === 1);
+      assert(envelope2.applyTo.callCount === 1);
+      assert(envelope2.applyTo.args[0][0] === op[OSCILLATOR].detune);
+      assert(envelope2.applyTo.args[0][1] === 1);
+      assert(envelope3.applyTo.callCount === 1);
+      assert(envelope3.applyTo.args[0][0] === op[OSCILLATOR].frequency);
+      assert(envelope3.applyTo.args[0][1] === 1);
+    });
+  });
+  describe("#getEnvelope(target: string = 'gain'): void", () => {
+    it("works", () => {
+      let op = new Operator(audioContext);
+      let envelope1 = Envelope.asr(0.2, 0.5, 0.3);
+      let envelope2 = envelope1.madd(5);
+      let envelope3 = envelope2.madd(10, 880);
+
+      op.setEnvelope(envelope1);
+      op.setEnvelope(envelope2, "detune");
+      op.setEnvelope(envelope3, "frequency");
+
+      assert(op.getEnvelope() === envelope1);
+      assert(op.getEnvelope("detune") === envelope2);
+      assert(op.getEnvelope("frequency") === envelope3);
+      assert(typeof op.getEnvelope("delayTime") === "undefined");
+    });
+  });
+});
